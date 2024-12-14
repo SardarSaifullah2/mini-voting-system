7d75fcd4783c6f68817401221a030afea14ab760
Sardar Saifullah <saifisardar5656@yahoo.com>
2024-12-14 11:09:48 +0500

    Implement election management features including candidate creation, election deletion, and voter blacklisting

--- index.c
+++ index.c
@@ -7,11 +7,32 @@ void AdminMenu();
 void Register();
 void Login();
 void AdminDashBoard();
-
+void CreateElection();
+int CheckedELection();
+int DeleteElection();
+void DeleteIllegalVote();
+void ViewElectionResult();
+void BlackList_Voter();
 struct User
 {
     char name[50];
     char password[50];
+};
+
+struct Candidate
+{
+    char name[50];
+    int vote;
+};
+
+struct BlackList_Voter
+{
+    char name[50];
+};
+
+struct Voter
+{
+    char name[50];
 };
 
 int main()
@@ -25,7 +46,7 @@ void Start()
     int choice;
     while (1)
     {
-        printf("\n1. Amdin Panel \n 2. Voter Panel \n 3. Exit \n Enter your choice: ");
+        printf("\n 1. Amdin Panel \n 2. Voter Panel \n 3. Exit \n Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice)
@@ -52,7 +73,7 @@ void AdminMenu()
     int choice;
     while (1)
     {
-        printf("\n1. Register \n 2. Login \n 3. Exit \n Enter your choice: ");
+        printf("\n 1. Register \n 2. Login \n 3. Exit \n Enter your choice: ");
         scanf("%d", &choice);
 
         switch (choice)
@@ -112,7 +133,7 @@ void Login()
     struct User user;
     FILE *file = fopen("D:\\semester 2\\programming fundamental\\project\\main project/admin.txt", "r");
 
-    printf("Enter Your Username : ");
+    printf(" Enter Your Username : ");
     scanf("%s", user.name);
     char name[50], password[50];
     int found = 0;
@@ -136,10 +157,9 @@ void Login()
     }
 
     fclose(file);
-
     if (found)
     {
-        printf("Login Successful\n");
+        printf("Login Successful......\n");
         AdminDashBoard();
     }
     else
@@ -153,7 +173,7 @@ void AdminDashBoard()
     int choice;
     while (1)
     {
-        printf("\n1. Create Election\n2. Delete Election\n3. Delete Illegal Votes\n4. View Election Result\n5. View Previous Election Results\n6. BlackList Voter\n7. Exit\nEnter your choice: ");
+        printf("\n 1. Create Election\n 2. Delete Election\n 3. Delete Illegal Votes\n 4. View Election Result\n 5. View Previous Election Results\n 6. BlackList Voter\n 7. Exit\nEnter your choice: ");
 
         scanf("%d", &choice);
 
@@ -161,21 +181,40 @@ void AdminDashBoard()
         {
         case 1:
             printf("Create ELection\n");
+            if (CheckedELection() == 1)
+            {
+                CreateElection();
+            }
+            else
+            {
+                printf("Election Already Created\n");
+            }
             break;
         case 2:
             printf("Delete Election\n");
+            if (DeleteElection() == 1)
+            {
+                printf("Election Deleted Successfully ...\n");
+            }
+            else
+            {
+                printf("Election Already Deleted\n");
+            }
             break;
         case 3:
             printf("Delete Illegal Votes\n");
+            DeleteIllegalVote();
             break;
         case 4:
             printf("View Election Result\n");
+            ViewElectionResult();
             break;
         case 5:
             printf("View Previous Election\n");
             break;
         case 6:
             printf("BlackList\n");
+            BlackList_Voter();
             break;
         case 7:
             printf("Exit\n");
@@ -184,5 +223,169 @@ void AdminDashBoard()
         default:
             printf("Invalid Choice\n");
         }
+    }
+}
+
+void CreateElection()
+{
+    int candidate_number;
+    char candidate_name[50];
+
+    printf("Enter the number of candidates : ");
+    scanf("%d", &candidate_number);
+
+    for (int i = 1; i <= candidate_number; i++)
+    {
+        FILE *file = fopen("D:\\semester 2\\programming fundamental\\project\\main project/candidate.txt", "a");
+        if (file == NULL)
+        {
+            printf("File not Open");
+            // return;
+        }
+        printf("Enter the name of candidate %d : ", i);
+        scanf("%s", candidate_name);
+        fprintf(file, "%s %d \n", candidate_name, 0);
+        fclose(file);
+        // return 1;
+    }
+    printf("Election Created Successfully........\n");
+}
+
+int CheckedELection()
+{
+    FILE *file = fopen("D:\\semester 2\\programming fundamental\\project\\main project/candidate.txt", "r");
+    fseek(file, 0, SEEK_END);
+
+    int fileSize = ftell(file);
+    fclose(file);
+    if (fileSize == 0)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int DeleteElection()
+{
+    FILE *file = fopen("D:\\semester 2\\programming fundamental\\project\\main project/candidate.txt", "w");
+    if (file == NULL)
+    {
+        printf("File not Open");
+        return 0;
+    }
+    fclose(file);
+    return 1;
+}
+
+void DeleteIllegalVote()
+{
+    struct User user[100];
+    int name, i = 0, choice, length;
+    char names[40];
+
+    FILE *file = fopen("D:\\semester 2\\programming fundamental\\project\\main project\\vote.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("File Not Found");
+    }
+
+    while (fscanf(file, "%s", names) != EOF)
+    {
+        strcpy(user[i].name, names);
+        i++;
+    }
+    length = i;
+
+    for (i = 0; i < length; i++)
+    {
+        printf("%d. %s\n", i + 1, user[i].name);
+    }
+    printf("Choose Option : ");
+    scanf("%d", &choice);
+    FILE *file_write = fopen("D:\\semester 2\\programming fundamental\\project\\main project\\vote.txt", "w");
+    for (i = 0; i < length; i++)
+    {
+        if (strcmp(user[i].name, user[choice - 1].name) != 0)
+        {
+            fprintf(file_write, "%s\n", user[i].name);
+        }
+    }
+    fclose(file_write);
+}
+
+void ViewElectionResult()
+{
+    printf("\n Inside View Election Result\n");
+    struct Candidate candidate[10];
+    int i = 0, length, vote;
+    char names[40];
+
+    FILE *file = fopen("D:\\semester 2\\programming fundamental\\project\\main project\\candidate.txt", "r");
+
+    if (file == NULL)
+    {
+        printf("File Not Found");
+    }
+
+    while (fscanf(file, "%s %d\n", candidate[i].name, &candidate[i].vote) != EOF)
+    {
+        i++;
+    }
+    fclose(file);
+
+    length = i;
+    printf("Length : %d", length);
+
+    for (i = 0; i < length; i++)
+    {
+        printf("\n %s %d \n", candidate[i].name, candidate[i].vote);
+        printf("\n Press Enter to Continue\n");
+        getchar();
+    }
+}
+
+void BlackList_Voter()
+{
+    int i = 0, length, choice;
+    struct BlackList_Voter blackList_voter[100];
+    struct Voter voter[100];
+
+    FILE *voter_file = fopen("D:\\semester 2\\programming fundamental\\project\\main project\\voter.txt", "r");
+    if (voter_file == NULL)
+    {
+        printf("File not Open");
+        return;
+    }
+
+    while (fscanf(voter_file, "%s %d", voter[i].name) != EOF)
+    {
+        printf("%s\n", voter[i].name);
+        i++;
+    }
+    fclose(voter_file);
+    length = i;
+    for (i = 0; i < length; i++)
+    {
+        printf("%d. %s\n", i + 1, voter[i].name);
+    }
+    printf("Choose Option : ");
+    scanf("%d", &choice);
+    // FILE *voter_file_write = fopen("D:\\semester 2\\programming fundamental\\project\\main project\\voter.txt", "w");
+    for (i = 0; i < length; i++)
+    {
+        // if (i != choice - 1)
+        if (strcmp(voter[i].name, voter[choice - 1].name) == 0)
+        {
+            FILE *blackList_file = fopen("D:\\semester 2\\programming fundamental\\project\\main project\\blacklist.txt", "a");
+            if (blackList_file == NULL)
+            {
+                printf("File not Open");
+                return;
+            }
+            fprintf(blackList_file, "%s\n", voter[choice - 1].name);
+            fclose(blackList_file);
+        }
+        // fclose(voter_file_write);
     }
 }
